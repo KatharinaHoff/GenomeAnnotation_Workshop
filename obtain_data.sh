36ef6b71670ae562a4b933e593d6cb9d22d3902d
KatharinaHoff <katharina.hoff@gmail.com>
2023-05-03 22:04:29 +0200

    implemented tapeworm notebook 50%

--- obtain_data.sh
+++ obtain_data.sh
@@ -6,6 +6,9 @@ cd busco
 wget https://busco-data.ezlab.org/v4/data/lineages/brassicales_odb10.2020-08-05.tar.gz
 tar -zxvf brassicales_odb10.2020-08-05.tar.gz
 rm brassicales_odb10.2020-08-05.tar.gz
+wget https://busco-data.ezlab.org/v4/data/lineages/metazoa_odb10.2020-09-10.tar.gz
+tar -zxvf metazoa_odb10.2020-09-10.tar.gz
+rm metazoa_odb10.2020-09-10.tar.gz
 cd ..
 mkdir orthodb
 cd orthodb
@@ -17,23 +20,22 @@ cd ..
 mkdir sra
 cd sra
 # Arabidopsis thaliana, only if anyone wants to try running Hisat themselves, probably not happening due to runtime
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR934/SRR934391/SRR934391_1.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR934/SRR934391/SRR934391_2.fastq.gz
-# output of Hisat2 for above named libraries
+# wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR934/SRR934391/SRR934391_1.fastq.gz
+# wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR934/SRR934391/SRR934391_2.fastq.gz
+# output of Hisat2 for above named libraries, could potentially be skipped but is mentioned in notebook
 wget https://bioinf.uni-greifswald.de/bioinf/katharina/krumlov/SRR934391.sam
-# Hymenolopis microstoma, only if anyone wants to try running Hisat themselves, probably not happening due to runtime
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR337/ERR337976/ERR337976_1.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR337/ERR337976/ERR337976_2.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR225/ERR225716/ERR225716_1.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR225/ERR225716/ERR225716_2.fastq.gz
-# ouput of Hisat2, and Samtools bam conversion, and 2 rounds of samtools sort for above named libraries
-wget https://bioinf.uni-greifswald.de/bioinf/katharina/krumlov/ERR225716.ss.bam
-wget https://bioinf.uni-greifswald.de/bioinf/katharina/krumlov/ERR337976.ss.bam
-# genome of Hymenolopis microstoma repeat masked (custom masking)
+# Hymenolopis microstoma
+# VARUS output
+wget https://bioinf.uni-greifswald.de/bioinf/katharina/krumlov/Merged.bam
+# genome of Hymenolopis microstoma repeat masked by NCBI
 cd ..
 mkdir genome
 cd genome
-wget https://bioinf.uni-greifswald.de/bioinf/katharina/krumlov/genome.fa.masked.gz
-gunzip genome.fa.masked.gz
-
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/469/805/GCA_000469805.3_HMN_v3/GCA_000469805.3_HMN_v3_genomic.fna.gz
+gunzip GCA_000469805.3_HMN_v3_genomic.fna.gz
+cd ..
+mkdir proteins
+cd proteins
+wget https://bioinf.uni-greifswald.de/bioinf/katharina/krumlov/proteins.fa.gz
+gunzip proteins.fa.gz
 # 59 GB of data in total
